c3ca345aac6317c786728538a098527405608607
Andres Giraldo <andresganc@gmail.com>
2019-12-02 21:36:44 -0500

    New: Numbers, OperadoresAritmeticos

---
+++ basicos/Numbers.js
@@ -0,0 +1,37 @@
+/* Numbers */
+// Los números son valores que se pueden usar en operaciones matemáticas. 
+// No necesita ninguna sintaxis especial para los números, 
+// simplemente escríbalos directamente en JavaScript.
+
+console.log (12345);
+
+/* Decimales y fracciones */
+// JavaScript no distingue entre números enteros y decimales, 
+// por lo que puede usarlos juntos sin tener que convertirlos de uno a otro.
+
+console.log (10 + 3.14159);
+//OUTPUT: 13.14159
+
+
+/* Las fracciones no existen en JavaScript,  */
+// pero puede reescribirlas como problemas de división usando el operador de división /. 
+// Tenga en cuenta que el número resultante siempre se convierte a decimales, al igual que con una calculadora.
+
+console.log (1 / 3);
+//OUTPUT: 0.3333333333333333
+
+// Las fracciones impropias usan el operador de división de la misma manera.
+console.log (11 / 10);
+//OUTPUT: 1.1
+
+// Para usar números mixtos, debe sumar el número entero y la fracción.
+console.log (1 + (4 / 3));
+//OUTPUT: 2.333333333333333
+
+/* Números negativos */
+// Puede hacer un número negativo colocando el operador - al frente.
+console.log (-3);
+
+// También puede obtener un número negativo restando un número de un número más pequeño.
+console.log (5 - 7);
+//OUTPUT: -2
--- basicos/OperadoresAritmeticos.js
+++ basicos/OperadoresAritmeticos.js
@@ -1,1 +1,34 @@
+
+/* Operadores */
+// Los operadores son los símbolos entre valores que permiten diferentes operaciones como suma, resta, multiplicación y más.
+// JavaScript tiene docenas de operadores, así que concentrémonos en los que probablemente verá con más frecuencia.
+
+// Aritmética
+// El operador + agrega dos números.
+
+console.log (1 + 2);
+// OUTPUT: 3
+
+// El operador - resta un número de otro.
+
+console.log (50 - 15);
+// OUTPUT: 35
+
+// El operador * multiplica dos números. Observe que es un asterisco y no el símbolo × comúnmente utilizado en matemáticas.
+
+console.log (3 * 12);
+// OUTPUT: 36
+
+//El operador / divide un número por otro. Observe que es una barra diagonal y no el símbolo ÷ comúnmente usado en matemáticas.
+
+console.log (12/4); 
+// OUTPUT: 3
+
+// Las expresiones de JavaScript siguen un orden de operaciones , 
+// por lo que aunque + se escribe primero en el siguiente ejemplo, 
+// la multiplicación ocurre primero entre los dos últimos números y *.
+console.log (1 + 100 * 5);
+// OUTPUT: 501
+
+// Aplicando varias operaciones
 console.log((100+4+8)*(5)-4);
