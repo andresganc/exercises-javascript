5bc53d4fd1493160ee92036f01f32cc80af4fe62
Andres Giraldo <andresganc@gmail.com>
2020-03-20 14:00:03 -0500

    Mod: Organizando el curso basico

---
+++ 1.Basicos/1.tipos-datos.js
@@ -0,0 +1,24 @@
+
+// ================================= TIPOS DE DATOS ======================================
+
+
+// 1. Es case sensitive
+// Distingue mayusculas de minusculas
+
+
+// 2. Tipado debil o Dinamico
+// Significa que las variables son del tipo de datos que almacenen
+// En una variable se pueden almacenar un texto y despues un numero o un boolean etc.
+
+
+// 3. Las sentencias finalizan con ;
+// Es recomendado. El interprete lo puede poner por nosotros pero en algunas ocaciones puede fallar y se produce un error.
+
+
+// 4. Los bloque finalizan con la llave de cierre }
+// De forma opcional se puede poner un ;
+// Algunos editores de codigo lo marcan como un error. (Aunque no es un error)
+// Funciona igual si se pone si no se pone
+
+
+
--- 1.Basicos/Ejercicios/sumas.js
+++ 1.Basicos/Ejercicios/sumas.js
@@ -16,6 +16,7 @@ console.log(numero1 + numero2);
 // Ejercicio 1
 // Pedir 3 numeros a un usuario, sumarlos y mostrar el resultado 
 
+
 console.log("SUMAR 3 NUMEROS");
 
 console.log("Inserte el primer numero:");
@@ -27,14 +28,14 @@ numero1.addListener("data", function(d){
 
 
 
-
+/*
 console.log("Inserte el segundo numero :");
 numero2 = process.openStdin();
 numero2.addListener("data", function(d){
     console.log("El numero que insertaste fue :" + d.toString().trim());
 });
 
-/*
+
 console.log("Inserte el tercer numero :");
 numero3 = process.openStdin();
 numero3.addListener("data", function(d){
