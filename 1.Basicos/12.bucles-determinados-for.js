348fb0ceee136732fd5f3e6c3acc3bb6ab1de6dd
Andres Giraldo <andresganc@gmail.com>
2020-03-25 17:59:43 -0500

    New: nota-media. Mod: for, while

--- 1.Basicos/12.bucles-determinados-for.js
+++ 1.Basicos/12.bucles-determinados-for.js
@@ -36,30 +36,34 @@ console.log("Ejemplo 2");
 for ( inicioBucle; inicioBucle<10; inicioBucle++ ){
 
     console.log(inicioBucle);
-    
+
 }
 
 // Ejemplo 3
 console.log("Ejemplo 3");
-for (var i=0; i<10; i+=2){
+
+for (var i=0; i<10; i+=2) {
+
     console.log(i);
+
 }
+
 
 // Ejemplo 4
 console.log("Ejemplo 4");
-for (var i=5; i<10; i++){
+
+for (var i=10; i>0; i--) {
+
     console.log(i);
+
 }
+
 
 // Ejemplo 5
 console.log("Ejemplo 5");
 
-for (var i=10; i>0; i--){
-    console.log(i);
-}
+for ( var i=0; i<=3; i++ ) {
 
-// Ejemplo 6
-console.log("Ejemplo 6");
-for (var i=0; i<=3; i++){
     console.log(ProductoNC[i]);
+
 }
--- 1.Basicos/13.bucles-indeterminados-while-doWhile.js
+++ 1.Basicos/13.bucles-indeterminados-while-doWhile.js
@@ -10,8 +10,8 @@
 /*
 SINTAXIS
 
-while () {
-
+while (condicion) {
+    haga esto
 }
 
 */
@@ -19,15 +19,42 @@ while () {
 var ProductoNC = ["Camara fija interna", "camara fija externa", "DVR - XVR", "Video Balum"];
 var meses = [
     "Enero", "Febrero", "Marzo",
-    "Abril", "Mayo", "Junio",
+    "Abril", "Mayo", "Junio",              
     "Julio", "Agosto", "Septiembre",
     "Octubre", "Noviembre", "Diciembre"];
 
 var contador = 0;
 
+// Ejemplo 1
+
+while (contador < 4){
+
+    console.log (ProductoNC[contador]);
+    //contador = contador + 1;      // Es lo mismo que poner contador ++
+    contador ++
+}
+
+contador.
+
+contador = 0;
+// Ejemplo 2
 while ( contador < meses.length ) {
 
     console.log (meses[contador]);
     contador ++
 
 }
+
+
+
+// BUCLE INDETERMINADO WHILE
+// Se usa cuando quiero que una accion se realice en su primera vez asi la condicion no se cumpla.
+
+/*
+SINTAXIS
+
+do {
+    esto
+}while (true);
+
+*/
