59c3fc6c6ec18df1e4f06e86fd52fea478f0fbb0
Andres Giraldo <andresganc@gmail.com>
2020-04-06 21:10:04 -0500

    Mod: POO.js, poo.html

---
+++ 2.Intermedio/14.POO.js
@@ -0,0 +1,83 @@
+import Boton from 'Button';
+
+// ========================= POO - Programacion Orientada a Objetos (OOP - Object-oriented programming) =======================
+
+// Existen 2 modelos de lenguajes de programacion
+
+// Programacion Orientado a Procedimientos: Lenguajes antiguos
+
+// Programacion Orientada a Objetos¨: Lenguajes modernos
+
+
+// PROGRAMACION ORIENTADO A OBJETOS
+
+// Caracteristicas y Capacidades
+
+// Carro
+
+// Caracteristicas (Propiedades): PuertaColor, TamañoRuedas, Ventanas, Motor. 
+
+// Capacidades (Metodos): Arrancar, Acelerar, Frenar, Girar.
+
+// Carro 1 (Familiar)
+// Propiedades              -               Metodos
+
+// puertaColor : Azules                     // Arrancar
+// ruedasTamaño : Pequeñas                  // Acelerar
+// ventanas : Cristal Pequeñas              // Frenar
+// motor : 1200cc                           // Girar
+
+
+// Carro 2 (Carreras)
+// Propiedades              -               Metodos
+
+// puertaColor : Gris                       // Arrancar   (Mas rapido)
+// ruedasTamaño: Grandes                    // Acelerar   (Mas rapido)
+// ventanas : Sin ventanas                  // Frenar     (Mas rapido)
+// motor : 10000cc                          // Girar      (Mas rapido)
+
+
+// Button de (HTML5 - React)
+// Propiedades                              Metodos
+
+// color:   Azul                            // Click                            
+// ancho:   100px                           // Focus
+// alto:    100px                           // Moverse
+// titulo:  Suma                            // Activo
+// bordes:  Redondeados                     // Inactivo
+
+
+
+
+// =========================== NOMENCLATURA DEL PUNTO =========================
+
+// ========= PROPIEDADES ===========
+
+// objeto.propiedad="";
+
+
+// Ej Objeto: carro1
+// carro1.puertaColor="Azul";
+
+// Ej Objeto: button
+// button.color="Azul";
+
+
+// ========= METODOS ===========
+
+// objeto.metodo();
+
+
+// Ej Objeto: carro1
+// carro1.arrancar();
+
+// Ej Objeto: button
+// button.click();
+
+
+// Usos reales dentro del lenguaje
+
+
+// console.log();
+// window.alert();
+
