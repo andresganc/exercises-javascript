36b0a84db015e2e8715c99d10089b7d564e46cca
Andres Giraldo <andresganc@gmail.com>
2020-05-06 00:26:52 -0500

    Mod: Arreglo de evento onclick por JavaScript en HTML5

--- 2.Intermedio/Ejercicios/Suma/js/suma.js
+++ 2.Intermedio/Ejercicios/Suma/js/suma.js
@@ -1,23 +1,47 @@
+
 // Esta linea de codigo asegura que la pagina esta cargada
-$(document).on("ready", suma());
+//$(document).on("ready", suma());
+
+var resultado;
+
+
+function main() {
+
+    var total = document.getElementById("LabelTotal");
+    var buttonSumar = document.getElementById("ButtonSumar");
+    var buttonBorrar = document.getElementById("ButtonBorrar");
+
+    // Paso 4.
+    buttonSumar.onclick = function(e) {
+        suma(); 
+        total.textContent = resultado;
+        e.preventDefault();
+    }
+
+    buttonBorrar.onclick = function(e) {
+        borrar();
+    }
+}
+
 
 function suma() {
-
     // Paso 1 & 2
     var numero1 = document.getElementById("InputNumero1").value;
 
     var numero2 = document.getElementById("InputNumero2").value;
 
     var numero3 = document.getElementById("InputNumero3").value;
-
+   
     // Paso 3
-    var resultado = ( parseFloat(numero1) + parseFloat(numero2) + parseFloat(numero3) );
+    resultado = ( parseFloat(numero1) + parseFloat(numero2) + parseFloat(numero3) );
+}
 
-    // Paso 5.
-    document.getElementById("LabelTotal").innerHTML = resultado
 
-    window.alert("El resultado de la suma es : " + resultado);
-
+function borrar(){
+    document.getElementById("InputNumero1").innerHTML = 0;
+    document.getElementById("InputNumero2").innerHTML = 0;
+    document.getElementById("InputNumero3").innerHTML = 0;
+    document.getElementById("LabelTotal").innerHTML = 0;    
 }
 
 
