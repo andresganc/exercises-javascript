50a0bd33c682af48f8b298541a59c147f912a62e
Andres Giraldo <andresganc@gmail.com>
2020-07-15 21:30:14 -0500

    Mod: Mayusculas a Minusculas

--- TypeScript/1.Type/type.js
+++
@@ -1,6 +0,0 @@
-var nombre = "Andres";
-var apellido;
-var cedula = 94474361;
-var sexo = "Masculino";
-console.log("Nombre " + nombre);
-console.log("Nombre: " + nombre + " Apellido: " + apellido + " ");
