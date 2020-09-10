36f74e68ac00253f039e118aef7fbba33e2d5e78
Andres Giraldo <andresganc@gmail.com>
2020-09-09 22:14:01 -0500

    Mod: Objetos

--- 1-basico/teoria/18.poo.js
+++ 1-basico/teoria/18.poo.js
@@ -1,12 +1,19 @@
-import Boton from 'Button';
 
 // ========================= POO - Programacion Orientada a Objetos (OOP - Object-oriented programming) =======================
 
-// Existen 2 modelos de lenguajes de programacion
+// Existen varios paradigmas de programacion
 
 // Programacion Orientado a Procedimientos: Lenguajes antiguos
 
+// Programacion Imperativa
+
+// Programacion Declarativa
+
+// Programacion Logica
+
 // Programacion Orientada a Objetos¨: Lenguajes modernos
+
+// Programacion orientada a Funciones : Lenguajes modernos
 
 
 // PROGRAMACION ORIENTADO A OBJETOS
@@ -83,4 +90,81 @@ import Boton from 'Button';
 
 // console.log();
 // window.alert();
+
+
+
+// ================================================================
+// SINTAXIS
+
+// Declarara objetos con el constructor - Casi no es usada
+const objetoConConstructor = new Object()
+
+//
+let objetoConLet = {
+
+}
+
+// Al declarar objetos con consts evito que otro objeto ocupe la referencia que en memoria 
+// Tiene este objeto.
+const objetoConConst = {
+
+}
+
+// ejemplo objeto
+const _94474361 = {
+    nombre: 'Andres',
+    apellido: 'Giraldo',
+    edad: 42,
+    hobbies: ['Siclo montañismo', 'Fotografia', 'Aviacion virtual'],
+    soltero: false,
+    celular: 3153076495,
+    email1: 'andresganc@gmail.com',
+    redes: {
+        facebook: '@andresganc',
+        instagram: '@andresganc',
+    },
+    saludar: function() {
+        console.log(`Hola :) mi nombre es : ${this.nombre} ${this.apellido} y tengo ${this.edad} años. Puedes seguirme en facebook: ${this.redes.facebook} `)
+    }
+}
+
+// Tipo 
+console.log(`Esto es un : ${typeof _94474361}`)
+
+// Mostrar todo el objeto con sus propiedades o atributos
+console.log(_94474361)
+
+// Llamando una propiedad del objeto - Forma 1 mas usada
+console.log(_94474361.nombre)
+
+// Llamando una propiedad del objeto - Forma 2
+console.log(_94474361 ['apellido'])
+
+// Llamando un metodo del objeto ( Las funciones dentro de un objeto se llaman metodos )
+_94474361.saludar()
+
+
+
+// ====================== OBJETOS ========================
+
+
+// ====================== METODOS ========================
+
+// Object.keys()
+// Metodo que lista las llaves de los objetos
+console.log(Object.keys(_94474361))
+
+
+// Object.values()
+// Metodo que lista los valores de un objeto
+console.log(Object.values(_94474361))
+
+
+// Object.hasOwnProperty()
+// Metodo para saber si la propiedad existe en el objeto
+console.log(_94474361.hasOwnProperty('apellido'))
+console.log(_94474361.hasOwnProperty('nacimiento'))
+
+
+
 
