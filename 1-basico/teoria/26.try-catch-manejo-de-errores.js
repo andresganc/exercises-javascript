eea9a4eeb42b2f9bd2a0ce6d5cfefeef8b31b641
Andres Giraldo <andresganc@gmail.com>
2020-09-15 23:32:49 -0500

    New: try-catch. Mod: Organizando orden

---
+++ 1-basico/teoria/26.try-catch-manejo-de-errores.js
@@ -0,0 +1,37 @@
+
+// TRY - CATCH - FINALLY
+// Metodos para capturar errores
+
+try {
+    console.log('En el try se agrega el codigo a evaluar');
+} catch(error) {
+    console.log('Si en el codigo del try hay un error el catch lo captura')
+} finally {
+    console.log('El bloque finally se ejecutara siempre al final de un bloque try catch')
+}
+
+// Ejemplo
+
+try {
+    noexiste
+} catch (error) {
+    console.log(`Error. catch ha capturado un error. El error es: ${error}`);
+    console.log('Ya no aparece el mensaje de error rojo en consola por que los errores ya los esta gestionando try catch');
+}
+
+// Lanzar un error
+try {
+
+    let numero = 10;
+    //let numero = 'Andres';        // Genera el error por no ser un numero isNaN
+
+    if (isNaN(numero)) {
+        throw new Error('El caracter introducido no es un numero');
+    }
+    console.log(numero * numero);
+
+} catch (error) {
+
+    console.log(`Se produjo el siguiente error: ${error}`);
+    
+}
