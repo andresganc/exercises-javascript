4f23459108b2d45bf4be4c2a05e1232b1df02d85
Andres Giraldo <andresganc@gmail.com>
2020-11-27 08:04:11 -0500

    Mod: Promises, Prueba main default

--- documentation/3-avanced/doc/promises/promises.js
+++ documentation/3-avanced/doc/promises/promises.js
@@ -1,4 +1,24 @@
 
-// 
+// =========== PROMISES ============
+/*
+- Las promesas se crearon para solucionar el codigo piramidal y poco legible
+    de los callback. Los famosos callback-hell 
 
-function requestHandler()
+- Las promesas son funciones con parametros req y res (Requerimiento - Respuesta) que tienen internamente un 
+.then si es ok
+.catch si es error
+
+*/
+
+
+function requestHandler(req, res) {
+    // Consulta User
+    User.findById(req.userId)
+        // Si ok realizo un .then (Entonces)
+        .then(function (user){
+            res.send(user)
+        })
+        .catch(function(err){
+            res.send(err)
+        })
+}
