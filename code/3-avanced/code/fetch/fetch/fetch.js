9ffa3e33cf1e97228257f6252ec6fc56c227af20
Andres Giraldo <andresganc@gmail.com>
2021-01-19 15:36:08 -0500

    New: fetch. Mod: Restructuring

---
+++ code/3-avanced/code/fetch/fetch/fetch.js
@@ -0,0 +1,11 @@
+
+/*
+FETCH - CONEXIONES API EN JAVASCRIPT
+
+- Fetch es una libreria nativa de javascript para hacer conexiones HTTP
+
+- No es necesario importar librerias externas como axios etc.
+
+- Devuelve una promesa por default pero tambien se puede trabajar con Async Await
+
+*/
