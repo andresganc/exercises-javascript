d5553636238bad48f473e844bdca8ad95b7846f9
Andres Giraldo <andresganc@gmail.com>
2021-05-25 23:38:09 -0500

    New: DOM, BOM CSSOM, APIS-WEB

---
+++ code/bom-browser-object-model/js/ventanas.js
@@ -0,0 +1,8 @@
+
+// View of browser
+console.log ( 'Size view browser - Width: ', window.innerWidth )
+console.log ( 'Size view browser - Width: ', window.innerHeight )
+
+// Size browser full
+console.log ( 'Size browser full - Width: ', window.outerWidth )
+console.log ( 'Size browser full - Width: ', window.outerHeight )
---
+++ code/dom-document-object-model/query-selector/js/query-selector.js
@@ -0,0 +1,12 @@
+
+// Seleccion del elemento h4 del DOM
+const queryElement = document.querySelector('h4')
+console.log(queryElement)
+
+// Seleccion de un elemento del DOM por clase
+const queryClass = document.querySelector('.query-class')
+console.log(queryClass)
+
+// Seleccion de un elemento del DOM por id
+const queryID = document.querySelector('#query-id')
+console.log(queryID)
