229ec3db74ae22bae14e7102a84838d84ce91ec5
Andres Giraldo <andresganc@gmail.com>
2021-06-25 20:11:38 -0500

    Mod: Button WC

---
+++ exercises/exercises-js/src/components/buttons/button-wc.js
@@ -0,0 +1,26 @@
+
+class ButtonWc extends HTMLElement {
+    constructor() {
+        super();
+        //implementation
+    }
+
+    connectedCallback() {
+        //implementation
+    }
+
+    disconnectedCallback() {
+        //implementation
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        //implementation
+    }
+
+    adoptedCallback() {
+        //implementation
+    }
+
+}
+
+window.customElements.define('button-wc', ButtonWc);
