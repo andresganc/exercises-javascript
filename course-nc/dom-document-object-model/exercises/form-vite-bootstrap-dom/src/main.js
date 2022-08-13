0de286783fdee8d87ef8b277d1b92c551d50baaa
Andres Giraldo <andresganc@gmail.com>
2022-08-13 10:25:49 -0500

    New: Exercice Form Dom Bootstrap

---
+++ course-nc/dom-document-object-model/exercises/form-vite-bootstrap-dom/src/main.js
@@ -0,0 +1,21 @@
+
+// Styles
+import './css/style.css'
+
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import Alert from 'bootstrap/js/dist/alert';
+
+// or, specify which plugins you need:
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+/*
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+*/
