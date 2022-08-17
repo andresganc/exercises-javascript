7c141d1d6de4509e38513eb5db90504163496a5f
Andres Giraldo Arenas <andresganc@gmail.com>
2022-08-17 09:04:25 -0500

    Del: Move to new repository course-dev-web

--- course-nc/bom-browser-object-model/js/window.js
+++
@@ -1,8 +0,0 @@
-
-// View of browser
-console.log ( 'Size view browser - Width: ', window.innerWidth )
-console.log ( 'Size view browser - Width: ', window.innerHeight )
-
-// Size browser full
-console.log ( 'Size browser full - Width: ', window.outerWidth )
-console.log ( 'Size browser full - Width: ', window.outerHeight )
--- course-nc/dom-document-object-model/course-nc/event-listener/js/event-listener.js
+++
@@ -1,9 +0,0 @@
-
-const ContainerMain = document.querySelector('.main')
-const H2Title = document.querySelector('.h2-title')
-const btnCambiar = document.querySelector('.btn-cambiar')
-
-btnCambiar.addEventListener('click', () => {
-    H2Title.classList.toggle('titleStyles')
-    ContainerMain.classList.toggle('mainStyles')
-})
