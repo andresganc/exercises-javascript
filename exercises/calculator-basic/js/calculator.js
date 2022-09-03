bb6895c861b75137013395c4c3f8555a9368d5c5
Andres Giraldo Arenas <andresganc@gmail.com>
2022-09-02 23:05:39 -0500

    Mod: AddEventlistener to btns & Display

--- exercises/calculator-basic/js/calculator.js
+++ exercises/calculator-basic/js/calculator.js
@@ -1,12 +1,34 @@
 
-export function basicOperator() {
+let numberCurrent
 
-    
+// GETS
+// Display
+const ContainertBottomValue = document.getElementById('display__bottom')
+// Buttons
+const btn1 = document.getElementById('btn-1')
+const btn2 = document.getElementById('btn-2')
 
+btn1.addEventListener("click", (e) => {
+    e.preventDefault()
+    numberCurrent = 1
+    display()
+});
+
+btn2.addEventListener("click", (e) => {
+    e.preventDefault()
+    numberCurrent = 2
+    display()
+});
+
+
+
+function display() {
+    const displayBottom = document.createElement('h1')
+    displayBottom.innerHTML = numberCurrent
+    ContainertBottomValue.appendChild(displayBottom)
 }
 
-export function buttons() {
 
-    const btn1 = document.querySelector('#btn1')
-    console.log(btn1)
+export function basicOperator() {
+
 }
--- exercises/calculator-basic/main.js
+++ exercises/calculator-basic/main.js
@@ -1,46 +1,11 @@
-import "./css/calculator.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
+
+
+
+
+
 
 document.querySelector("#app").innerHTML = `
-<div class='calculator'>
-
-<div class='display'>
-  <div class='display__top'>
-    <h3> 0 </h3>
-  </div>
-
-  <div class='display__bottom'>
-    <h1> 0 </h1>
-  </div>
-</div >
-
-<div class='buttons'>
-
-  <div id='btn-ac' name='btn-ac' class='buttons__btn'>AC</div>
-  <div class='buttons__btn'>+/-</div>
-  <div class='buttons__btn'>%</div>
-  <div class='buttons__btn-operator'>/</div>
-  <div id='btn-1' name='btn-7' class='buttons__btn'>7</div>
-  <div id='btn-1' name='btn-8' class='buttons__btn'>8</div>
-  <div id='btn-1' name='btn-9' class='buttons__btn'>9</div>
-  <div class='buttons__btn-operator'>X</div>
-  <div id='btn-1' name='btn-4' class='buttons__btn'>4</div>
-  <div id='btn-1' name='btn-5' class='buttons__btn'>5</div>
-  <div id='btn-1' name='btn-6' class='buttons__btn'>6</div>
-  <div class='buttons__btn-operator'>-</div>
-  <div id='btn-1' name='btn-1' class='buttons__btn'>1</div>
-  <div id='btn-2' name='btn-2' class='buttons__btn'>2</div>
-  <div id='btn-1' name='btn-1' class='buttons__btn'>3</div>
-  <div class='buttons__btn-operator'>+</div>
-  <div class='buttons__btn'>0</div>
-  <div class='buttons__btn'>0</div>
-  <div class='buttons__btn'>,</div>
-  <div class='buttons__btn-operator'>=</div>
-
-</div>
-  
-</div>
+  <h2> CALCULATOR - BASIC </h2>
 `;
 
 setupCounter(document.querySelector("#counter"));
