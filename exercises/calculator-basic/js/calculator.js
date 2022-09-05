005afd2c334d6622239874f2474b67193e835873
Andres Giraldo <andresganc@gmail.com>
2022-09-05 10:36:44 -0500

    Mod: Bug addEventListener Buttons Ok

--- exercises/calculator-basic/js/calculator.js
+++ exercises/calculator-basic/js/calculator.js
@@ -10,30 +10,30 @@ displayCurrent()
 displayBefore()
 
 // Buttons
-const btn0 = document.getElementById('btn-0')
-const btn1 = document.getElementById('btn-1')
-const btn2 = document.getElementById('btn-2')
-const btn3 = document.getElementById('btn-3')
-const btn4 = document.getElementById('btn-4')
-const btn5 = document.getElementById('btn-5')
-const btn6 = document.getElementById('btn-6')
-const btn7 = document.getElementById('btn-7')
-const btn8 = document.getElementById('btn-8')
-const btn9 = document.getElementById('btn-9')
+const btn0 = document.getElementById('btn-0');
+const btn1 = document.getElementById('btn-1');
+const btn2 = document.getElementById('btn-2');
+const btn3 = document.getElementById('btn-3');
+const btn4 = document.getElementById('btn-4');
+const btn5 = document.getElementById('btn-5');
+const btn6 = document.getElementById('btn-6');
+const btn7 = document.getElementById('btn-7');
+const btn8 = document.getElementById('btn-8');
+const btn9 = document.getElementById('btn-9');
 
-btn0.addEventListener("click", (e) => {
+btn0.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 0
     displayCurrent()
 });
 
-btn1.addEventListener("click", (e) => {
+btn1.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 1
     displayCurrent()
 });
 
-btn2.addEventListener("click", (e) => {
+btn2.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 2
     displayCurrent()
@@ -43,43 +43,43 @@ btn3.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 3
     displayCurrent()
-})
+});
 
 btn4.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 4
     displayCurrent()
-})
+});
 
 btn5.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 5
     displayCurrent()
-})
+});
 
 btn6.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 6
     displayCurrent()
-})
+});
 
 btn7.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 7
     displayCurrent()
-})
+});
 
 btn8.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 8
     displayCurrent()
-})
+});
 
 btn9.addEventListener('click', (e) => {
     e.preventDefault()
     numberCurrent = 9
     displayCurrent()
-})
+});
 
 
 function displayCurrent() {
@@ -94,7 +94,6 @@ function displayBefore() {
     containertDisplayBefore.appendChild(displayBefore)
 }
 
-
-export function basicOperator() {
+function basicOperator() {
 
 }
