fa8ed80c405b1429dbb45fbf03124e5b181319d6
Andres Giraldo Arenas <andresganc@gmail.com>
2022-12-17 10:49:43 -0500

    Restructuring

---
+++ course-nc/2-advanced/code/use-strict/accidental-global-variables/example-with-use-strict.js
@@ -0,0 +1,14 @@
+
+// EXAMPLE WITH USE STRICT
+
+/* 
+In this example we are intentionally making a syntax error and javascript detects it because 'use strict' is being used
+
+If we try to run it in a web browser, node or deno will not detect the error
+*/
+
+'use strict'
+
+let name = 'Andres'
+
+names = 'Error de sintaxis intencional'
---
+++ course-nc/2-advanced/code/use-strict/accidental-global-variables/example-without-use-strict.js
@@ -0,0 +1,12 @@
+
+
+// EXAMPLE WITHOUT USE STRICT
+
+/* 
+In this example we are intentionally making a syntax error and javascript does not detect 
+it even if a new global variable is created with the name "names" which can cause many problems in the code. 
+*/
+
+let name = 'Andres'
+
+names = 'Error de sintaxis intencional'
