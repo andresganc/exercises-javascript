a3d204eabc3b899c0490831d0a865339e6326609
Andres Giraldo <andresganc@gmail.com>
2023-12-13 20:30:22 -0500

    Mod: Todo List Basic

--- exercises/01-basic/todo-lists/todo-list-basic/TodoListBasic.js
+++ exercises/01-basic/todo-lists/todo-list-basic/TodoListBasic.js
@@ -4,15 +4,38 @@ class TodoListBasic extends HTMLElement {
   constructor() {
     super()
     this.attachShadow({mode: 'open'})
+    const props = [...this.attributes].map(props => {})
+    console.log(props)
+    this.state = {}
+  
+    this.saludo = 'Saludo NC'
   }
+
+  serState(() {
+    
+  })
 
   connectedCallback() {
     this.render()
+    this.innerHTML = this.saludo
+    this.innerHTML = this.changeInput
+  }
+
+
+
+  changeInput() {
+    console.log('Hola')
   }
 
   render() {
     this.shadowRoot.innerHTML = `
       <h2>Todo List Basic</h2>
+      <input type="text" placeholder='Add to list'/>
+      <button type="button" onclick='${this.changeInput}'>+</button>
+
+      <div>
+        <h4>Task List</h4>
+      </div>
     `
   }
 
@@ -20,6 +43,7 @@ class TodoListBasic extends HTMLElement {
     return `
       :host {
         display: block;
+        justify-content: center;
       }
     `
   }
